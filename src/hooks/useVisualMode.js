0f75ce44dd22bdda3a3a10d7e166220f7bb5e9fd
Hoang Dinh <hoangdinh90@gmail.com>
2020-02-19 17:48:19 -0800

    functions pass tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
-
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  const [history, setHistory] = useState([initial]);
+  const [history, setHistory] = useState([]);
+  console.log('here is history:', history)
 
-  function transition(mode) {
-    setMode(mode)
+  function transition(input, replace) {
+    if (!replace) {
+      setHistory(prev => [...prev, mode]);
+    }
+    setMode(input)
   }
 
-
   function back() {
-
+    if (history.length >= 1) {
+      setMode(history[history.length - 1]);
+      setHistory(prev => [...prev.slice(0, -1)]);
+    }
   }
 
   return { mode, transition, back }
